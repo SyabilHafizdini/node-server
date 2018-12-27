a3aa62da0e310bb208e91c55a3531693c3db1684
Swagbil <Syabilsatification@gmail.com>
2018-12-27 09:33:22 +0800

    fixing pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,11 +7,11 @@ module.exports = {
       production: {
         user: 'ubuntu',
         host: 'ec2-54-169-130-7.ap-southeast-1.compute.amazonaws.com',
-        key: '~/.ssh/nodeserver.pem',
+        key: './nodeserver.pem',
         ref: 'origin/master',
         repo: 'git@github.com:SyabilHafizdini/node-server.git',
         path: '/home/ubuntu/node-server',
-        'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+        'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js && touch yeetpe'
       }
     }
   }
